c68ca890561a1ddfcd5ee0cea62f30241fafa40d
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2023-10-11 09:47:05 +0700

    [UPDATE] update sql script

--- dbScript.sql
+++ dbScript.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id serial PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
+    retailer_name VARCHAR(255) NOT NULL,
 	bplus_code VARCHAR(255) NOT NULL,
 	mobile_number VARCHAR(10) NOT NULL
 	
 );
 
-INSERT INTO users (user_id, username, bplus_code, mobile_number)
+INSERT INTO users (user_id, retailer_name, bplus_code, mobile_number)
 VALUES
     ('1', 'user1', 'ABC123', '1234567890'),
     ('2', 'user2', 'DEF456', '9876543210'),
