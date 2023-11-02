234dfaa533d6a7ccb6e949e33d6b2f6128922893
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2023-11-02 16:17:04 +0700

    [UPDATE] cus in fo

--- src/queries/adminQueries.js
+++ src/queries/adminQueries.js
@@ -1,4 +1,4 @@
 const getLogin = "SELECT * FROM Admins WHERE username = ? AND password = ?";
 const getCustomerInfoList = `SELECT * from customer_info;`;
-
+ 
 module.exports = { getLogin, getCustomerInfoList };
